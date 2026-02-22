7e32182a4b9c61e8eea3d79e0147d7d86f495749
Lilian Zapata <lilia.zapata@gmail.com>
2026-02-22 20:14:04 -0300

    last

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 // 2. Luego CSS (para que las reglas como .btn-primary ganen a las de Bootstrap)
-import 'assets/css/estilos.css' 
+import './assets/css/estilos.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
